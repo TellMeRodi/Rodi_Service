7a3f8e8ee7dc47c4c80358c6e4540deb04676b1c
hhjjoa2817 <hhjjoa2817@naver.com>
2024-02-21 10:11:57 +0900

    Question, Choice, Response 모델링 및 base.html 구조작성

--- Researches/models.py
+++ Researches/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=200)
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=200)
+
+class Response(models.Model):
+    session_key = models.CharField(max_length=40)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
