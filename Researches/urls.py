ade38aaac5e354d78483a2f507e66b98284cc376
bhj8286 <bjs219@naver.com>
2024-02-22 16:35:40 +0900

    update

--- Researches/urls.py
+++ Researches/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from .views import research
+
+
 
 app_name = 'Researches'
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('research/', views.research, name='research'),
+    
 ]
