50b655fcb7b6ed4da93c4b6013f8b289832ac3a4
Dhyeonle9 <dhyeon.le9@gmail.com>
2024-02-26 15:33:37 +0900

    api 연결 views.py/urls.py 수정

--- Researches/views.py
+++ Researches/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Question
+import requests
 
 # Create your views here.
 def home(request):
@@ -75,8 +76,45 @@ def MBTI_JP(request):
             style8=request.session['style8'],
             MBTI=MBTI,
         )
-        return redirect('Researches:result')
+        return redirect('Researches:recommendations')
     return render(request, 'MBTI_JP.html')
 
-def result(request):
-    return render(request, 'result.html')
+def definite_data(session):
+    MBTI_EI = session.get('MBTI_EI')
+    MBTI_SN = session.get('MBTI_SN')
+    MBTI_FT = session.get('MBTI_FT')
+    MBTI_JP = session.get('MBTI_JP')
+
+    MBTI = MBTI_EI + MBTI_SN + MBTI_FT + MBTI_JP
+    
+    data = {
+        'age': session.get('age'),
+        'gender': session.get('gender'),
+        'style1': session.get('style1'),
+        'style2': session.get('style2'),
+        'style3': session.get('style3'),
+        'style4': session.get('style4'),
+        'style5': session.get('style5'),
+        'style6': session.get('style6'),
+        'style7': session.get('style7'),
+        'style8': session.get('style8'),
+        'MBTI': MBTI,
+    }
+    return data
+
+def get_recommendations(request):
+    data = definite_data(request.session)
+    # FastAPI 엔드포인트 URL
+    fastapi_url = "http://localhost:8001/home/recommendations/"
+    
+    # 장고 뷰로 받은 데이터를 FastAPI 엔드포인트로 전달
+    response = requests.post(fastapi_url, json=data)  # 수정된 부분
+    
+    # FastAPI의 응답 처리
+    if response.status_code == 200:
+        recommendations = response.json().get('recommended_cities', [])
+        context = {'recommendations': recommendations}
+    else:
+        context = {'error': 'API 호출에 실패했습니다.'}
+
+    return render(request, 'result.html', context)
