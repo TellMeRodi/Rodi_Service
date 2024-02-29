281f6c0989d0dc1cddb2a8ba3218642c1c89bb63
bhj8286 <bjs219@naver.com>
2024-02-29 11:11:38 +0900

    폰트적용/ 버튼 색상 설정 및 변경/ 배경 이미지 삽입/ 요소 배치 조정

--- RODI/settings.py
+++ RODI/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 from . import local_settings
 
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "Researches",
+    'django_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -112,9 +113,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# 미디어 경로를 처리할 URL
+MEDIA_URL = '/media/'
+
+
